ce0d31ce017996d6aa71f1b1c1011c7ae4c7c17a
AndrewVanardennen <Andrew.Vanardennen@gmail.com>
2018-04-06 22:49:28 +0200

    Add Dynamic Assets

---
+++ Source/S05_TestingGrounds/Terrain/Tile.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class S05_TESTINGGROUNDS_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "PlaceActor")
+	void PlaceActors(TSubclassOf<AActor>ToSpawn, int32 MinSpawn = 1, int32 MaxSpawn = 1, float Radius = 500);
+
+private:
+	bool FindEmptyLocation(FVector& OutLocation, float Radius);
+	
+	void PlaceActor(TSubclassOf<AActor>ToSpawn, FVector SpawnPoint, float Rotation);
+
+	bool CanSpawnAtLocation(FVector Location, float Radius);
+};
